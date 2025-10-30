e14f75ce4c93ae97aef22d35417839ab942d1ebf
Ahmed Salman <ahmedsalman@Ahmeds-MacBook-Air.local>
2025-10-30 13:35:18 +0530

    ..

--- cafeaura-website/src/App.jsx
+++ cafeaura-website/src/App.jsx
@@ -2,30 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-<<<<<<< HEAD
 import Home from './pages/Home'
 import Header from './components/Header'
-import Footer from './components/Footer'
-=======
-import Header from './components/Header'
-import Home from './pages/Home'
 import Footer from './components/Footer'
 
->>>>>>> origin/bhuvan-dev
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-<<<<<<< HEAD
     <>
     <Header />
-=======
-    <><Header />
->>>>>>> origin/bhuvan-dev
+
 <Home />
 <Footer />
     </>
   )
 }
 
-export default App
+export default App;
