dfbfbd3dfc3600f62183dde43d8bc2bb6a9bd567
Ahmed Salman <ahmedsalman@Ahmeds-MacBook-Air.local>
2025-10-30 10:07:05 +0530

    sa

--- cafeaura-website/src/App.jsx
+++ cafeaura-website/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Home from './pages/Home'
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header />
 <Home />
+<Footer />
     </>
   )
 }
