5a3074e60f0ff9be235318b4cdcf77840fb3837a
Your Name <you@example.com>
2025-10-30 10:04:41 +0530

    added utube video

--- cafeaura-website/src/App.jsx
+++ cafeaura-website/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <><Header />
 <Home />
+<Footer />
     </>
   )
 }
