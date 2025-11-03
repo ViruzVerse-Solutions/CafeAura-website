8ea23644a1833de359f3f5a29b4fe5e520fcf789
Arunkumar0526 <victorarun32@gmail.com>
2025-11-03 10:10:48 +0530

    about page_1

--- cafeaura-website/src/App.jsx
+++ cafeaura-website/src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import About from './pages/About'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-<Home />
+    <Header/>
+    <About/>
+    <Footer/>
     </>
   )
 }
 
-export default App
+export default App;
